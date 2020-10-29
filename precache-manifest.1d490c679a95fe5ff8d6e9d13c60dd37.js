2b97db5649000ff49307efa0dc6ff07faecb3fe6
Chris <smurf123444@gmail.com>
2020-10-29 14:18:46 -0400

    TITS

---
+++ precache-manifest.1d490c679a95fe5ff8d6e9d13c60dd37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "/home/static/media/farmer.e0a371e7.png"
+  },
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "/home/static/media/dai.e0a371e7.png"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "3d7f8124ec470f138273",
+    "url": "/home/static/js/main.3d7f8124.chunk.js"
+  },
+  {
+    "revision": "7f8809bf56c7df6112ba",
+    "url": "/home/static/js/1.7f8809bf.chunk.js"
+  },
+  {
+    "revision": "3d7f8124ec470f138273",
+    "url": "/home/static/css/main.cce09b36.chunk.css"
+  },
+  {
+    "revision": "7f8809bf56c7df6112ba",
+    "url": "/home/static/css/1.c74ed7a1.chunk.css"
+  },
+  {
+    "revision": "454f3f527eb53666d7217024ef5a1392",
+    "url": "/home/index.html"
+  }
+];
