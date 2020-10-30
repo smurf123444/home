2957d721f62a382ff476c4c199566a8ebcc12f53
oem <smurf123444@gmail.com>
2020-10-30 14:29:55 -0400

    fixed static

--- precache-manifest.3f25aa21a33a6e65a3320adf35f29226.js
+++ precache-manifest.3f25aa21a33a6e65a3320adf35f29226.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = [
   {
     "revision": "e0a371e78decccec11fe158c42b708e5",
-    "url": "/static/media/farmer.e0a371e7.png"
+    "url": "./static/media/farmer.e0a371e7.png"
   },
   {
     "revision": "e0a371e78decccec11fe158c42b708e5",
-    "url": "/static/media/dai.e0a371e7.png"
+    "url": "./static/media/dai.e0a371e7.png"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "./static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "fae2433e7c6a16a1c78a",
-    "url": "/static/js/main.fae2433e.chunk.js"
+    "url": "./static/js/main.fae2433e.chunk.js"
   },
   {
     "revision": "85d29d742c9b3599b51f",
-    "url": "/static/js/1.85d29d74.chunk.js"
+    "url": "./static/js/1.85d29d74.chunk.js"
   },
   {
     "revision": "fae2433e7c6a16a1c78a",
-    "url": "/static/css/main.141c9bc5.chunk.css"
+    "url": "./static/css/main.141c9bc5.chunk.css"
   },
   {
     "revision": "85d29d742c9b3599b51f",
-    "url": "/static/css/1.02915418.chunk.css"
+    "url": "./static/css/1.02915418.chunk.css"
   },
   {
     "revision": "5b6c59f069f823262d0599c404546ddc",
