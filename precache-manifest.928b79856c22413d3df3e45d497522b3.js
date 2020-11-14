8c309bc1df82a8be842b0833bca360f70db59afd
Chris <smurf123444@gmail.com>
2020-11-14 05:40:36 -0500

    added old site

---
+++ precache-manifest.928b79856c22413d3df3e45d497522b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "/static/media/farmer.e0a371e7.png"
+  },
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "/static/media/dai.e0a371e7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a71b38e3a217c1e7027a",
+    "url": "/static/js/main.a71b38e3.chunk.js"
+  },
+  {
+    "revision": "7cb7976bbc65271220af",
+    "url": "/static/js/1.7cb7976b.chunk.js"
+  },
+  {
+    "revision": "a71b38e3a217c1e7027a",
+    "url": "/static/css/main.1c6f1a97.chunk.css"
+  },
+  {
+    "revision": "7cb7976bbc65271220af",
+    "url": "/static/css/1.0d07e61f.chunk.css"
+  },
+  {
+    "revision": "536164d684af5a355e3089646baddb69",
+    "url": "/index.html"
+  }
+];
