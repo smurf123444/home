700ef8fa287e2eb3984a97687da4dc57c1b572be
Chris <smurf123444@gmail.com>
2020-11-14 05:58:30 -0500

    added new contract

---
+++ precache-manifest.6d1f2005389629b96b63efe9390903ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "/static/media/farmer.e0a371e7.png"
+  },
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "/static/media/dai.e0a371e7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8a4918ff6a1c6b25d03",
+    "url": "/static/js/main.f8a4918f.chunk.js"
+  },
+  {
+    "revision": "7cb7976bbc65271220af",
+    "url": "/static/js/1.7cb7976b.chunk.js"
+  },
+  {
+    "revision": "f8a4918ff6a1c6b25d03",
+    "url": "/static/css/main.1c6f1a97.chunk.css"
+  },
+  {
+    "revision": "7cb7976bbc65271220af",
+    "url": "/static/css/1.0d07e61f.chunk.css"
+  },
+  {
+    "revision": "9313921a5e021608d863b7cd1aed0344",
+    "url": "/index.html"
+  }
+];
