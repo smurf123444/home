3ac136224837945f5a8930305e93a1d2914a5389
Coder <smurf123444@gmail.com>
2021-02-02 18:15:47 -0500

    Update precache-manifest.532252e47c59620e0adfb8e7b266089c.js

--- precache-manifest.532252e47c59620e0adfb8e7b266089c.js
+++ precache-manifest.532252e47c59620e0adfb8e7b266089c.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "fc4de2245285a981f4e7",
-    "url": "/static/css/main.d0b66179.chunk.css"
+    "url": "./static/css/main.d0b66179.chunk.css"
   },
   {
     "revision": "fc4de2245285a981f4e7",
-    "url": "/static/js/main.fc4de224.chunk.js"
+    "url": "./static/js/main.fc4de224.chunk.js"
   },
   {
     "revision": "14223a2346386cf4bec6",
-    "url": "/static/css/1.b1ed7d9a.chunk.css"
+    "url": "./static/css/1.b1ed7d9a.chunk.css"
   },
   {
     "revision": "14223a2346386cf4bec6",
-    "url": "/static/js/1.14223a23.chunk.js"
+    "url": "./static/js/1.14223a23.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "./static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "e0a371e78decccec11fe158c42b708e5",
-    "url": "/static/media/dai.e0a371e7.png"
+    "url": "./static/media/dai.e0a371e7.png"
   },
   {
     "revision": "55028bd5a7380c37466e626a5025cf8c",
-    "url": "/index.html"
+    "url": "./index.html"
   }
 ];
